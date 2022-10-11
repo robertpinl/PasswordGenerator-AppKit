39262b5acc2456c29a1d757aa5f9a9f36ef4ccfb
Robert Pinl <robert.pinl@icloud.com>
2022-10-11 20:22:07 +1300

    Initial Commit

---
+++ Password/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Robert P on 11.10.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
